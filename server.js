179b567997834ada986a29b19dc1dccc74d91f2f
jeffery.raimundo@medaire.com <jeff_pepper23@yahoo.com>
2019-07-20 13:22:08 -0700

    still heroku bugs

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const path = require("path");
 //const db = require("./models");
 
 const app = express();
-const PORT = process.env.PORT || 7500;
+const PORT = process.env.PORT || 3001;
 
 app.use(compression());
 // app.use(morgan("common"));
